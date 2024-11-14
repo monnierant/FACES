00129c2f22502c33691c0ea3a49e15ba7410aaca
Monnier Antoine <monnier.ant@free.fr>
2024-11-14 18:59:27 +0100

    feat: Use the correct rule for exploding dices

--- src/ts/apps/documents/FacesActor.ts
+++ src/ts/apps/documents/FacesActor.ts
@@ -71,52 +71,88 @@ export default class FacesActor extends Actor {
   }
 
   public async rollExplode(
-    dice: number,
     difficulty: number,
-    previousResult: number,
-    weaponId: number | undefined,
-    weaponIsMelee: boolean,
-    weaponBonus: number,
-    remainingExplodes: number[]
+    double: number,
+    modificator: number,
+    dices: {
+      previousResult: number;
+      previousDice: number;
+      otherResult: number;
+      otherDice: number;
+    },
+    weapon: {
+      weaponId: number | undefined;
+      weaponIsMelee: boolean;
+      weaponBonus: number;
+    },
+    isDoubleExplode: boolean
   ) {
-    const roll = await new Roll(`1d${dice}`).roll();
+    const secondRoll = isDoubleExplode ? `+1d${dices.otherDice}` : "";
+    const roll = await new Roll(`1d${dices.previousDice}${secondRoll}`).roll();
+    const isOneA =
+      roll.dice[0].results[0].result == 1 &&
+      game.settings?.get(moduleIdCore, "oneisfaill");
+    const isOneT = isDoubleExplode
+      ? roll.dice[1].results[0].result == 1 &&
+        game.settings?.get(moduleIdCore, "oneisfaill")
+      : false;
 
-    roll.dice.forEach((d) => {
-      if (d.results[0].result == d.faces) {
-        remainingExplodes.push(d.faces);
-      }
-    });
+    const valueA = isOneA
+      ? 1
+      : roll.dice[0].results[0].result + dices.previousResult;
+    const valueT = isOneT
+      ? 1
+      : (isDoubleExplode ? roll.dice[1].results[0].result : 0) +
+        dices.otherResult;
 
-    const result =
-      roll.total == 1 && game.settings?.get(moduleIdCore, "oneisfaill")
-        ? 1
-        : roll.total + previousResult;
+    const max = Math.max(valueA, valueT);
+
+    const result = max + modificator + double;
+
+    const criticalFaill = valueA == 1 && valueT == 1;
+
     const success =
       result >= difficulty &&
-      (roll.total != 1 || !game.settings?.get(moduleIdCore, "oneisfaill"));
+      (roll.total != 1 || !game.settings?.get(moduleIdCore, "oneisfaill")) &&
+      !criticalFaill;
+
     const degree = success ? Math.floor((result - difficulty) / 4) : 0;
 
     const content = await renderTemplate(
       `systems/${moduleId}/templates/chat/explode.hbs`,
       {
         actor: this,
-        degree: degree,
         difficulty: difficulty,
+        modificator: modificator,
+        double: double,
         roll: roll,
         result: result,
+        degree: degree,
         success: success,
         faill: roll.total == 1,
-        previousResult: previousResult,
+        max: max,
+        rA: {
+          result: valueA,
+          faces: dices.previousDice,
+          canExplode: roll.dice[0].faces == roll.dice[0].results[0].result,
+        },
+        rT: {
+          result: valueT,
+          faces: dices.otherDice,
+          canExplode: isDoubleExplode
+            ? roll.dice[1].faces == roll.dice[1].results[0].result
+            : false,
+        },
         weapon:
-          weaponId !== undefined
-            ? this.getWeapon(weaponId, weaponIsMelee)
+          weapon.weaponId !== undefined
+            ? this.getWeapon(weapon.weaponId, weapon.weaponIsMelee)
             : undefined,
-        weaponId: weaponId,
-        weaponIsMelee: weaponIsMelee,
-        weaponBonus: weaponBonus,
-        canExplodes: remainingExplodes,
+        weaponId: weapon.weaponId,
+        weaponIsMelee: weapon.weaponIsMelee,
+        weaponBonus: weapon.weaponBonus,
         moduleId: moduleId,
         oneisfaill: game.settings?.get(moduleIdCore, "oneisfaill"),
+        criticalFaill: criticalFaill,
       }
     );
 
@@ -154,13 +190,8 @@ export default class FacesActor extends Actor {
       }
     }
 
-    let canExplodes: number[] = [];
     let max = 0;
     roll.dice.forEach((d) => {
-      if (d.results[0].result == d.faces) {
-        canExplodes.push(d.faces);
-      }
-
       if (d.results[0].result > max) {
         max = d.results[0].result;
       }
@@ -174,23 +205,32 @@ export default class FacesActor extends Actor {
       `systems/${moduleId}/templates/chat/roll.hbs`,
       {
         actor: this,
-        degree: degree,
+        weaponId: weaponId,
+        weaponIsMelee: weaponIsMelee,
+        weaponBonus: weaponBonus,
+        double: double,
+        moduleId: moduleId,
         difficulty: difficulty,
         modificator: modificator,
+        degree: degree,
         roll: roll,
-        max: max,
+        rA: {
+          result: roll.dice[0].results[0].result,
+          faces: roll.dice[0].faces,
+          canExplode: roll.dice[0].faces == roll.dice[0].results[0].result,
+        },
+        rT: {
+          result: roll.dice[1].results[0].result,
+          faces: roll.dice[1].faces,
+          canExplode: roll.dice[1].faces == roll.dice[1].results[0].result,
+        },
         result: result,
+        max: max,
         success: success,
         weapon:
           weaponId !== undefined
             ? this.getWeapon(weaponId, weaponIsMelee)
             : undefined,
-        weaponId: weaponId,
-        weaponIsMelee: weaponIsMelee,
-        weaponBonus: weaponBonus,
-        double: double,
-        canExplodes: canExplodes,
-        moduleId: moduleId,
         criticalFaill: criticalFaill,
       }
     );
